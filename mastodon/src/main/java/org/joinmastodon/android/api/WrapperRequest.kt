09c316ed366fb8d7bc5b9a6102b61ba867f40995
FelipeIgansi <lipeigansi@gmail.com>
2025-03-06 22:10:56 -0300

    Migration java to kotlin: WrapperRequest

--- mastodon/src/main/java/org/joinmastodon/android/api/WrapperRequest.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/WrapperRequest.kt
@@ -1,21 +1,19 @@
-package org.joinmastodon.android.api;
+package org.joinmastodon.android.api
 
-import me.grishka.appkit.api.APIRequest;
+import me.grishka.appkit.api.APIRequest
 
 /**
  * Wraps a different API request to allow a chain of requests to be canceled
  */
-public class WrapperRequest<T> extends APIRequest<T>{
-	public APIRequest<?> wrappedRequest;
+class WrapperRequest<T> : APIRequest<T>() {
+    @JvmField
+		var wrappedRequest: APIRequest<*>? = null
 
-	@Override
-	public void cancel(){
-		if(wrappedRequest!=null)
-			wrappedRequest.cancel();
-	}
+    override fun cancel() {
+        if (wrappedRequest != null) wrappedRequest!!.cancel()
+    }
 
-	@Override
-	public APIRequest<T> exec(){
-		throw new UnsupportedOperationException();
-	}
+    override fun exec(): APIRequest<T>? {
+        throw UnsupportedOperationException()
+    }
 }
