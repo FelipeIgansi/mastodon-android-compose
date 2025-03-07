7e1fe00bf22c116cbacec9bf81b03ad7a1829577
FelipeIgansi <lipeigansi@gmail.com>
2025-03-06 22:22:26 -0300

    Migration java to kotlin: IsoInstantTypeAdapter

--- mastodon/src/main/java/org/joinmastodon/android/api/gson/IsoInstantTypeAdapter.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/gson/IsoInstantTypeAdapter.kt
@@ -1,45 +1,49 @@
-package org.joinmastodon.android.api.gson;
+package org.joinmastodon.android.api.gson
 
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonToken
+import com.google.gson.stream.JsonWriter
+import java.io.IOException
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.time.temporal.TemporalAccessor
+import java.time.temporal.TemporalQuery
 
-import java.io.IOException;
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+class IsoInstantTypeAdapter : TypeAdapter<Instant?>() {
+  @Throws(IOException::class)
+  override fun write(out: JsonWriter, value: Instant?) {
+    if (value == null) out.nullValue()
+    else out.value(DateTimeFormatter.ISO_INSTANT.format(value))
+  }
 
-public class IsoInstantTypeAdapter extends TypeAdapter<Instant>{
-	@Override
-	public void write(JsonWriter out, Instant value) throws IOException{
-		if(value==null)
-			out.nullValue();
-		else
-			out.value(DateTimeFormatter.ISO_INSTANT.format(value));
-	}
+  @Throws(IOException::class)
+  override fun read(reader: JsonReader): Instant? {
+    if (reader.peek() == JsonToken.NULL) {
+      reader.nextNull()
+      return null
+    }
+    val nextString = try {
+      reader.nextString()
+    } catch (e: Exception) {
+      return null
+    }
 
-	@Override
-	public Instant read(JsonReader in) throws IOException{
-		if(in.peek()==JsonToken.NULL){
-			in.nextNull();
-			return null;
-		}
-		String nextString;
-		try {
-			nextString = in.nextString();
-		}catch(Exception e){
-			return null;
-		}
-
-		try{
-			return DateTimeFormatter.ISO_INSTANT.parse(nextString, Instant::from);
-		}catch(DateTimeParseException x){}
-
-		try{
-			return DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(nextString, Instant::from);
-		}catch(DateTimeParseException x){}
-
-		return null;
-	}
+    return try {
+      DateTimeFormatter.ISO_INSTANT.parse<Instant?>(
+        nextString,
+        TemporalQuery { temporal: TemporalAccessor? -> Instant.from(temporal) }
+      )
+    } catch (x: DateTimeParseException) {
+      try {
+        return DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse<Instant?>(
+          nextString,
+          TemporalQuery { temporal: TemporalAccessor? -> Instant.from(temporal) }
+        )
+      } catch (x: DateTimeParseException) {
+        null
+      }
+    }
+  }
 }
