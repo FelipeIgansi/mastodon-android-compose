60c6457fe178677c5349eb2aac485a0bed75663c
FelipeIgansi <lipeigansi@gmail.com>
2025-03-06 22:00:42 -0300

    Migration java to kotlin: ProgressListener

--- mastodon/src/main/java/org/joinmastodon/android/api/ProgressListener.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/ProgressListener.kt
@@ -1,5 +1,5 @@
-package org.joinmastodon.android.api;
+package org.joinmastodon.android.api
 
-public interface ProgressListener{
-	void onProgress(long transferred, long total);
+interface ProgressListener {
+    fun onProgress(transferred: Long, total: Long)
 }
