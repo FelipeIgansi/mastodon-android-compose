67d035b80ba81de863dd3b6940c2a4056b02ab4b
FelipeIgansi <lipeigansi@gmail.com>
2025-03-06 22:14:39 -0300

    Migration java to kotlin: IsoLocalDateTypeAdapter

--- mastodon/src/main/java/org/joinmastodon/android/api/gson/IsoLocalDateTypeAdapter.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/gson/IsoLocalDateTypeAdapter.kt
@@ -1,33 +1,31 @@
-package org.joinmastodon.android.api.gson;
+package org.joinmastodon.android.api.gson
 
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonToken
+import com.google.gson.stream.JsonWriter
+import java.io.IOException
+import java.time.LocalDate
+import java.time.format.DateTimeParseException
 
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+class IsoLocalDateTypeAdapter : TypeAdapter<LocalDate?>() {
+  @Throws(IOException::class)
+  override fun write(out: JsonWriter, value: LocalDate?) {
+    if (value == null) out.nullValue()
+    else out.value(value.toString())
+  }
 
-public class IsoLocalDateTypeAdapter extends TypeAdapter<LocalDate>{
-	@Override
-	public void write(JsonWriter out, LocalDate value) throws IOException{
-		if(value==null)
-			out.nullValue();
-		else
-			out.value(value.toString());
-	}
-
-	@Override
-	public LocalDate read(JsonReader in) throws IOException{
-		if(in.peek()==JsonToken.NULL){
-			in.nextNull();
-			return null;
-		}
-		try{
-			return LocalDate.parse(in.nextString());
-		}catch(DateTimeParseException x){
-			return null;
-		}
-	}
+  @Throws(IOException::class)
+  override fun read(reader: JsonReader): LocalDate? {
+    if (reader.peek() == JsonToken.NULL) {
+      reader.nextNull()
+      return null
+    }
+    return try {
+      LocalDate.parse(reader.nextString())
+    } catch (e: DateTimeParseException) {
+      // Log the error or handle it as needed
+      null
+    }
+  }
 }
