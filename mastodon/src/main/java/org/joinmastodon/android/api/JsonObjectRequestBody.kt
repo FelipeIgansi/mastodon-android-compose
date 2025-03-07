a1c14847de2059d0bfeb9ef3488f76b41a078efb
FelipeIgansi <lipeigansi@gmail.com>
2025-03-06 21:58:14 -0300

    Migration java to kotlin: JsonObjectRequestBody

--- mastodon/src/main/java/org/joinmastodon/android/api/JsonObjectRequestBody.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/JsonObjectRequestBody.kt
@@ -1,39 +1,32 @@
-package org.joinmastodon.android.api;
+package org.joinmastodon.android.api
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonIOException;
+import com.google.gson.JsonElement
+import com.google.gson.JsonIOException
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okio.BufferedSink
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
+class JsonObjectRequestBody(private val obj: Any) : RequestBody() {
+    override fun contentType(): MediaType? {
+        return MediaType.get("application/json")
+    }
 
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okio.BufferedSink;
+    @Throws(IOException::class)
+    override fun writeTo(sink: BufferedSink) {
+        try {
+            val writer = OutputStreamWriter(sink.outputStream(), StandardCharsets.UTF_8)
 
-public class JsonObjectRequestBody extends RequestBody{
-	private final Object obj;
+            if (obj is JsonElement)
+                writer.write(obj.toString())
+            else
+                MastodonAPIController.gson.toJson(obj, writer)
 
-	public JsonObjectRequestBody(Object obj){
-		this.obj=obj;
-	}
-
-	@Override
-	public MediaType contentType(){
-		return MediaType.get("application/json");
-	}
-
-	@Override
-	public void writeTo(BufferedSink sink) throws IOException{
-		try{
-			OutputStreamWriter writer=new OutputStreamWriter(sink.outputStream(), StandardCharsets.UTF_8);
-			if(obj instanceof JsonElement)
-				writer.write(obj.toString());
-			else
-				MastodonAPIController.gson.toJson(obj, writer);
-			writer.flush();
-		}catch(JsonIOException x){
-			throw new IOException(x);
-		}
-	}
+            writer.flush()
+        } catch (x: JsonIOException) {
+            throw IOException(x)
+        }
+    }
 }
