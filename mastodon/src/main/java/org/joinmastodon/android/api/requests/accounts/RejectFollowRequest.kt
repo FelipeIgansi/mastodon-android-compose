17aa560d877f2585453e8534dfa7f759649596db
FelipeIgansi <lipeigansi@gmail.com>
2025-04-05 16:41:39 -0300

    Migrate: RejectFollowRequest by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/RejectFollowRequest.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/RejectFollowRequest.kt
@@ -1,11 +1,14 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Relationship;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Relationship
 
-public class RejectFollowRequest extends MastodonAPIRequest<Relationship> {
-	public RejectFollowRequest(String accountID) {
-		super(HttpMethod.POST, "/follow_requests/" + accountID + "/reject", Relationship.class);
-		setRequestBody(new Object());
-	}
+class RejectFollowRequest(accountID: String) : MastodonAPIRequest<Relationship>(
+  HttpMethod.POST,
+  "/follow_requests/$accountID/reject",
+  Relationship::class.java
+) {
+  init {
+    setRequestBody(Any())
+  }
 }
