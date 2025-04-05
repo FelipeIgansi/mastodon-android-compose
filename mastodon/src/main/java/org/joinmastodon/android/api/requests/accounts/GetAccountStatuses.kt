abbeb770c661951e4f38a8c357d605f68b03db5f
FelipeIgansi <lipeigansi@gmail.com>
2025-04-05 14:54:32 -0300

    Migrate: GetAccountStatuses by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetAccountStatuses.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetAccountStatuses.kt
@@ -1,46 +1,50 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import android.text.TextUtils;
+import android.text.TextUtils
+import com.google.gson.reflect.TypeToken
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Status
 
-import com.google.gson.reflect.TypeToken;
+class GetAccountStatuses(
+  id: String,
+  maxID: String?,
+  minID: String?,
+  limit: Int,
+  filter: Filter,
+  hashtag: String?
+) : MastodonAPIRequest<MutableList<Status>>(
+  HttpMethod.GET,
+  "/accounts/$id/statuses",
+  object : TypeToken<MutableList<Status>>() {}
+) {
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Status;
+  init {
+    maxID?.let { addQueryParameter("max_id", it) }
+    minID?.let { addQueryParameter("min_id", it) }
 
-import java.util.List;
+    if (limit > 0) addQueryParameter("limit", "$limit")
 
-import androidx.annotation.NonNull;
+    when (filter) {
+      Filter.DEFAULT -> addQueryParameter("exclude_replies", "true")
+      Filter.INCLUDE_REPLIES -> {}
+      Filter.MEDIA -> addQueryParameter("only_media", "true")
+      Filter.NO_REBLOGS -> {
+        addQueryParameter("exclude_replies", "true")
+        addQueryParameter("exclude_reblogs", "true")
+      }
 
-public class GetAccountStatuses extends MastodonAPIRequest<List<Status>>{
-	public GetAccountStatuses(String id, String maxID, String minID, int limit, @NonNull Filter filter, String hashtag){
-		super(HttpMethod.GET, "/accounts/"+id+"/statuses", new TypeToken<>(){});
-		if(maxID!=null)
-			addQueryParameter("max_id", maxID);
-		if(minID!=null)
-			addQueryParameter("min_id", minID);
-		if(limit>0)
-			addQueryParameter("limit", ""+limit);
-		switch(filter){
-			case DEFAULT -> addQueryParameter("exclude_replies", "true");
-			case INCLUDE_REPLIES -> {}
-			case MEDIA -> addQueryParameter("only_media", "true");
-			case NO_REBLOGS -> {
-				addQueryParameter("exclude_replies", "true");
-				addQueryParameter("exclude_reblogs", "true");
-			}
-			case OWN_POSTS_AND_REPLIES -> addQueryParameter("exclude_reblogs", "true");
-			case PINNED -> addQueryParameter("pinned", "true");
-		}
-		if(!TextUtils.isEmpty(hashtag))
-			addQueryParameter("tagged", hashtag);
-	}
+      Filter.OWN_POSTS_AND_REPLIES -> addQueryParameter("exclude_reblogs", "true")
+      Filter.PINNED -> addQueryParameter("pinned", "true")
+    }
+    if (!TextUtils.isEmpty(hashtag)) addQueryParameter("tagged", hashtag)
+  }
 
-	public enum Filter{
-		DEFAULT,
-		INCLUDE_REPLIES,
-		MEDIA,
-		NO_REBLOGS,
-		OWN_POSTS_AND_REPLIES,
-		PINNED
-	}
+  enum class Filter {
+    DEFAULT,
+    INCLUDE_REPLIES,
+    MEDIA,
+    NO_REBLOGS,
+    OWN_POSTS_AND_REPLIES,
+    PINNED
+  }
 }
