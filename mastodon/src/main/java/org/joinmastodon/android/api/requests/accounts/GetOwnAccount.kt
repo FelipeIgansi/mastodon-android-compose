acd4e49939aa4b696aaf756362f38279357f1bd5
FelipeIgansi <lipeigansi@gmail.com>
2025-04-05 15:36:00 -0300

    Migrate: GetOwnAccount by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetOwnAccount.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetOwnAccount.kt
@@ -1,10 +1,10 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Account;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Account
 
-public class GetOwnAccount extends MastodonAPIRequest<Account>{
-	public GetOwnAccount(){
-		super(HttpMethod.GET, "/accounts/verify_credentials", Account.class);
-	}
-}
+class GetOwnAccount : MastodonAPIRequest<Account>(
+  HttpMethod.GET,
+  "/accounts/verify_credentials",
+  Account::class.java
+)
