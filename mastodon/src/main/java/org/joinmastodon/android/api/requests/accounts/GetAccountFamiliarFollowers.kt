133cdd10b58c8f707b103155bc2c2c407f159fac
FelipeIgansi <lipeigansi@gmail.com>
2025-04-05 12:17:34 -0300

    Migrate: GetAccountFamiliarFollowers by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetAccountFamiliarFollowers.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetAccountFamiliarFollowers.kt
@@ -1,18 +1,21 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.reflect.TypeToken
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.FamiliarFollowers
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.FamiliarFollowers;
-
-import java.util.Collection;
-import java.util.List;
-
-public class GetAccountFamiliarFollowers extends MastodonAPIRequest<List<FamiliarFollowers>>{
-	public GetAccountFamiliarFollowers(Collection<String> ids){
-		super(HttpMethod.GET, "/accounts/familiar_followers", new TypeToken<>(){});
-		for(String id:ids){
-			addQueryParameter("id[]", id);
-		}
-	}
+class GetAccountFamiliarFollowers(ids: Collection<String>) :
+/**
+ * No código original (Java) é utilizado o List, porem o java não faz diferença de mutavel para imutavel,
+ * enquanto o Kotlin sim, portanto coloquei para melhor compatibilidade como mutavel.
+ * */
+  MastodonAPIRequest<MutableList<FamiliarFollowers>>(
+    HttpMethod.GET,
+    "/accounts/familiar_followers",
+    object : TypeToken<MutableList<FamiliarFollowers>>() {}) {
+  init {
+    ids.forEach { id ->
+      addQueryParameter("id[]", id)
+    }
+  }
 }
