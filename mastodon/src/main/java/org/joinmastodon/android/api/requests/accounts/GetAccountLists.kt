049ef5bc9410d53625ff0a572bef86ca0eb4aa2b
FelipeIgansi <lipeigansi@gmail.com>
2025-04-05 13:56:01 -0300

    Migrate: GetAccountLists by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetAccountLists.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/GetAccountLists.kt
@@ -1,14 +1,12 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import com.google.gson.reflect.TypeToken;
+import com.google.gson.reflect.TypeToken
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.FollowList
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.FollowList;
-
-import java.util.List;
-
-public class GetAccountLists extends MastodonAPIRequest<List<FollowList>>{
-	public GetAccountLists(String id){
-		super(HttpMethod.GET, "/accounts/"+id+"/lists", new TypeToken<>(){});
-	}
-}
+class GetAccountLists(id: String) :
+  MastodonAPIRequest<MutableList<FollowList>>(
+    HttpMethod.GET,
+    "/accounts/$id/lists",
+    object : TypeToken<MutableList<FollowList>>() {}
+  )
