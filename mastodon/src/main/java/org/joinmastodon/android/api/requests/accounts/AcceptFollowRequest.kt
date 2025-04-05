69fe515a519470407266bcbb567f2864d9a11dea
FelipeIgansi <lipeigansi@gmail.com>
2025-04-05 10:05:07 -0300

    Migrate: AcceptFollowRequest by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/AcceptFollowRequest.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/AcceptFollowRequest.kt
@@ -1,11 +1,15 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Relationship;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Relationship
 
-public class AcceptFollowRequest extends MastodonAPIRequest<Relationship> {
-	public AcceptFollowRequest(String accountID) {
-		super(HttpMethod.POST, "/follow_requests/" + accountID + "/authorize", Relationship.class);
-		setRequestBody(new Object());
-	}
+class AcceptFollowRequest(accountID: String) :
+  MastodonAPIRequest<Relationship>(
+    HttpMethod.POST,
+    "/follow_requests/$accountID/authorize",
+    Relationship::class.java
+  ) {
+  init {
+    setRequestBody(Any())
+  }
 }
