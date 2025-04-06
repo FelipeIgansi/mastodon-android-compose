c5a30dc26ac3a8601236f403a369bd04ebd72e71
FelipeIgansi <lipeigansi@gmail.com>
2025-04-06 11:27:54 -0300

    Migrate: UpdateAccountCredentialsPreferences by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/UpdateAccountCredentialsPreferences.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/UpdateAccountCredentialsPreferences.kt
@@ -1,35 +1,40 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Account;
-import org.joinmastodon.android.model.Preferences;
-import org.joinmastodon.android.model.StatusPrivacy;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Account
+import org.joinmastodon.android.model.Preferences
+import org.joinmastodon.android.model.StatusPrivacy
 
-public class UpdateAccountCredentialsPreferences extends MastodonAPIRequest<Account>{
-	public UpdateAccountCredentialsPreferences(Preferences preferences, Boolean locked, Boolean discoverable, Boolean indexable){
-		super(HttpMethod.PATCH, "/accounts/update_credentials", Account.class);
-		setRequestBody(new Request(locked, discoverable, indexable, new RequestSource(preferences.postingDefaultVisibility, preferences.postingDefaultLanguage)));
-	}
+class UpdateAccountCredentialsPreferences(
+  preferences: Preferences,
+  locked: Boolean?,
+  discoverable: Boolean?,
+  indexable: Boolean?
+) : MastodonAPIRequest<Account>(
+  HttpMethod.PATCH,
+  "/accounts/update_credentials",
+  Account::class.java
+) {
+  init {
+    val requestSource = RequestSource(
+      preferences.postingDefaultVisibility,
+      preferences.postingDefaultLanguage
+    )
 
-	private static class Request{
-		public Boolean locked, discoverable, indexable;
-		public RequestSource source;
+    val requestBody = Request(locked, discoverable, indexable, requestSource)
 
-		public Request(Boolean locked, Boolean discoverable, Boolean indexable, RequestSource source){
-			this.locked=locked;
-			this.discoverable=discoverable;
-			this.indexable=indexable;
-			this.source=source;
-		}
-	}
+    setRequestBody(requestBody)
+  }
 
-	private static class RequestSource{
-		public StatusPrivacy privacy;
-		public String language;
+  private data class Request(
+    val locked: Boolean?,
+    val discoverable: Boolean?,
+    val indexable: Boolean?,
+    val source: RequestSource
+  )
 
-		public RequestSource(StatusPrivacy privacy, String language){
-			this.privacy=privacy;
-			this.language=language;
-		}
-	}
+  private data class RequestSource(
+    val privacy: StatusPrivacy,
+    val language: String
+  )
 }
