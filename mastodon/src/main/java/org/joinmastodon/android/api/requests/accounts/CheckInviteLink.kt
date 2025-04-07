2e04af2b060536c965d9ab31cfe43e293bef4378
FelipeIgansi <lipeigansi@gmail.com>
2025-04-06 21:23:52 -0300

    Migrate: CheckInviteLink by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/CheckInviteLink.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/CheckInviteLink.kt
@@ -1,22 +1,23 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.api.RequiredField;
-import org.joinmastodon.android.model.BaseModel;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.api.RequiredField
+import org.joinmastodon.android.model.BaseModel
 
-public class CheckInviteLink extends MastodonAPIRequest<CheckInviteLink.Response>{
-	public CheckInviteLink(String path){
-		super(HttpMethod.GET, path, Response.class);
-		addHeader("Accept", "application/json");
-	}
+class CheckInviteLink(path: String) :
+  MastodonAPIRequest<CheckInviteLink.Response>(
+    HttpMethod.GET, path, Response::class.java
+  ) {
 
-	@Override
-	protected String getPathPrefix(){
-		return "";
-	}
+  init {
+    addHeader("Accept", "application/json")
+  }
 
-	public static class Response extends BaseModel{
-		@RequiredField
-		public String inviteCode;
-	}
+  protected override fun getPathPrefix() = ""
+
+  class Response (
+    @RequiredField
+    @JvmField
+    var inviteCode: String = ""
+  ): BaseModel()
 }
