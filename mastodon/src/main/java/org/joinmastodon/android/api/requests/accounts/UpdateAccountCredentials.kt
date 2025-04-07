2574bbaa70d8124360f22a331ef77dce1f52a49f
FelipeIgansi <lipeigansi@gmail.com>
2025-04-06 21:04:50 -0300

    Migrate: UpdateAccountCredentials by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/UpdateAccountCredentials.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/accounts/UpdateAccountCredentials.kt
@@ -1,86 +1,139 @@
-package org.joinmastodon.android.api.requests.accounts;
+package org.joinmastodon.android.api.requests.accounts
 
-import android.net.Uri;
+import android.net.Uri
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.joinmastodon.android.api.AvatarResizedImageRequestBody
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.api.ResizedImageRequestBody
+import org.joinmastodon.android.model.Account
+import org.joinmastodon.android.model.AccountField
+import org.joinmastodon.android.ui.utils.UiUtils
+import java.io.File
+import java.io.IOException
 
-import org.joinmastodon.android.api.AvatarResizedImageRequestBody;
-import org.joinmastodon.android.api.ContentUriRequestBody;
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.api.ResizedImageRequestBody;
-import org.joinmastodon.android.model.Account;
-import org.joinmastodon.android.model.AccountField;
-import org.joinmastodon.android.ui.utils.UiUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 
-public class UpdateAccountCredentials extends MastodonAPIRequest<Account>{
-	private String displayName, bio;
-	private Uri avatar, cover;
-	private File avatarFile, coverFile;
-	private List<AccountField> fields;
-	private Boolean discoverable, indexable;
+class UpdateAccountCredentials : MastodonAPIRequest<Account> {
+  private val displayName: String
+  private val bio: String
+  private var avatar: Uri? = null
+  private var cover: Uri? = null
+  private var avatarFile: File? = null
+  private var coverFile: File? = null
+  private val fields: List<AccountField>?
+  private var discoverable: Boolean? = null
+  private var indexable: Boolean? = null
+  
 
-	public UpdateAccountCredentials(String displayName, String bio, Uri avatar, Uri cover, List<AccountField> fields){
-		super(HttpMethod.PATCH, "/accounts/update_credentials", Account.class);
-		this.displayName=displayName;
-		this.bio=bio;
-		this.avatar=avatar;
-		this.cover=cover;
-		this.fields=fields;
-	}
+  constructor(
+    displayName: String,
+    bio: String,
+    avatar: Uri?,
+    cover: Uri?,
+    fields: List<AccountField>?
+  ) : super(HttpMethod.PATCH, "/accounts/update_credentials", Account::class.java) {
+    this.displayName = displayName
+    this.bio = bio
+    this.avatar = avatar
+    this.cover = cover
+    this.fields = fields
+  }
 
-	public UpdateAccountCredentials(String displayName, String bio, File avatar, File cover, List<AccountField> fields){
-		super(HttpMethod.PATCH, "/accounts/update_credentials", Account.class);
-		this.displayName=displayName;
-		this.bio=bio;
-		this.avatarFile=avatar;
-		this.coverFile=cover;
-		this.fields=fields;
-	}
+  constructor(
+    displayName: String,
+    bio: String,
+    avatar: File?,
+    cover: File?,
+    fields: List<AccountField>?
+  ) : super(HttpMethod.PATCH, "/accounts/update_credentials", Account::class.java) {
+    this.displayName = displayName
+    this.bio = bio
+    this.avatarFile = avatar
+    this.coverFile = cover
+    this.fields = fields
+  }
 
-	public UpdateAccountCredentials setDiscoverableIndexable(boolean discoverable, boolean indexable){
-		this.discoverable=discoverable;
-		this.indexable=indexable;
-		return this;
-	}
+  fun setDiscoverableIndexable(
+    discoverable: Boolean,
+    indexable: Boolean
+  ): UpdateAccountCredentials {
+    this.discoverable = discoverable
+    this.indexable = indexable
+    return this
+  }
 
-	@Override
-	public RequestBody getRequestBody() throws IOException{
-		MultipartBody.Builder bldr=new MultipartBody.Builder()
-				.setType(MultipartBody.FORM)
-				.addFormDataPart("display_name", displayName)
-				.addFormDataPart("note", bio);
+  @Throws(IOException::class)
+  override fun getRequestBody(): RequestBody {
+    val builder = MultipartBody.Builder().apply {
+      setType(MultipartBody.FORM)
+      addFormDataPart("display_$NAME", displayName)
+      addFormDataPart("note", bio)
 
-		if(avatar!=null){
-			bldr.addFormDataPart("avatar", UiUtils.getFileName(avatar), new AvatarResizedImageRequestBody(avatar, null));
-		}else if(avatarFile!=null){
-			bldr.addFormDataPart("avatar", avatarFile.getName(), new AvatarResizedImageRequestBody(Uri.fromFile(avatarFile), null));
-		}
-		if(cover!=null){
-			bldr.addFormDataPart("header", UiUtils.getFileName(cover), new ResizedImageRequestBody(cover, 1500*500, null));
-		}else if(coverFile!=null){
-			bldr.addFormDataPart("header", coverFile.getName(), new ResizedImageRequestBody(Uri.fromFile(coverFile), 1500*500, null));
-		}
-		if(fields!=null){
-			if(fields.isEmpty()){
-				bldr.addFormDataPart("fields_attributes[0][name]", "").addFormDataPart("fields_attributes[0][value]", "");
-			}else{
-				int i=0;
-				for(AccountField field:fields){
-					bldr.addFormDataPart("fields_attributes["+i+"][name]", field.name).addFormDataPart("fields_attributes["+i+"][value]", field.value);
-					i++;
-				}
-			}
-		}
-		if(discoverable!=null)
-			bldr.addFormDataPart("discoverable", discoverable.toString());
-		if(indexable!=null)
-			bldr.addFormDataPart("indexable", indexable.toString());
 
-		return bldr.build();
-	}
+      avatar?.let {
+        addFormDataPart(
+          AVATAR,
+          UiUtils.getFileName(it),
+          AvatarResizedImageRequestBody(it, null)
+        )
+      } ?:
+      avatarFile?.let {
+        addFormDataPart(
+          AVATAR,
+          it.name,
+          AvatarResizedImageRequestBody(Uri.fromFile(it), null)
+        )
+      }
+
+      val maxSize = 1500 * 500
+      
+      cover?.let {
+        addFormDataPart(
+          HEADER,
+          UiUtils.getFileName(it),
+          ResizedImageRequestBody(it, maxSize, null)
+        )
+      } ?:
+      coverFile?.let {
+        addFormDataPart(
+          HEADER,
+          it.name, 
+          ResizedImageRequestBody(Uri.fromFile(it), maxSize, null)
+        )
+      }
+
+
+      fields?.let {
+        if (fields.isEmpty()) {
+          addFormDataPart("$FIELDS_ATTRIBUTES[0][$NAME]", "")
+          addFormDataPart("$FIELDS_ATTRIBUTES[0][$VALUE]", "")
+        } else {
+          fields.forEachIndexed { index, field ->
+            addFormDataPart("$FIELDS_ATTRIBUTES[$index][$NAME]", field.name)
+            addFormDataPart("$FIELDS_ATTRIBUTES[$index][$VALUE]", field.value)
+          }
+        }
+      }
+
+      discoverable?.let { addFormDataPart(DISCOVERABLE, it.toString()) }
+      indexable?.let { addFormDataPart(INDEXABLE, it.toString()) }
+    }
+
+
+    return builder.build()
+  }
 }
+
+
+private const val AVATAR = "avatar"
+private const val HEADER = "header"
+
+private const val FIELDS_ATTRIBUTES = "fields_attributes"
+
+private const val NAME = "name"
+private const val VALUE = "value"
+
+private const val DISCOVERABLE = "discoverable"
+private const val INDEXABLE = "indexable"
