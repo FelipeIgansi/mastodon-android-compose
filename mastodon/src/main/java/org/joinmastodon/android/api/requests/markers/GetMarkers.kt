d89d2adfcb276c876136569d291707b9a23f9d52
FelipeIgansi <felipemigansi@gmail.com>
2025-07-19 18:38:03 -0300

    Migrate: GetMarkers by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/markers/GetMarkers.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/markers/GetMarkers.kt
@@ -1,12 +1,15 @@
-package org.joinmastodon.android.api.requests.markers;
+package org.joinmastodon.android.api.requests.markers
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.TimelineMarkers;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.TimelineMarkers
 
-public class GetMarkers extends MastodonAPIRequest<TimelineMarkers>{
-	public GetMarkers(){
-		super(HttpMethod.GET, "/markers", TimelineMarkers.class);
-		addQueryParameter("timeline[]", "home");
-		addQueryParameter("timeline[]", "notifications");
-	}
+class GetMarkers : MastodonAPIRequest<TimelineMarkers>(
+    method = HttpMethod.GET,
+    path = "/markers",
+    respClass = TimelineMarkers::class.java
+) {
+    init {
+        addQueryParameter("timeline[]", "home")
+        addQueryParameter("timeline[]", "notifications")
+    }
 }
