3a47145fc17265678d30cf22b382e6cf10712a53
FelipeIgansi <felipemigansi@gmail.com>
2025-07-19 20:10:53 -0300

    Migrate: SaveMarkers by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/markers/SaveMarkers.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/markers/SaveMarkers.kt
@@ -1,17 +1,34 @@
-package org.joinmastodon.android.api.requests.markers;
+package org.joinmastodon.android.api.requests.markers
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.api.gson.JsonObjectBuilder;
-import org.joinmastodon.android.model.TimelineMarkers;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.api.gson.JsonObjectBuilder
+import org.joinmastodon.android.model.TimelineMarkers
 
-public class SaveMarkers extends MastodonAPIRequest<TimelineMarkers>{
-	public SaveMarkers(String lastSeenHomePostID, String lastSeenNotificationID){
-		super(HttpMethod.POST, "/markers", TimelineMarkers.class);
-		JsonObjectBuilder builder=new JsonObjectBuilder();
-		if(lastSeenHomePostID!=null)
-			builder.add("home", new JsonObjectBuilder().add("last_read_id", lastSeenHomePostID));
-		if(lastSeenNotificationID!=null)
-			builder.add("notifications", new JsonObjectBuilder().add("last_read_id", lastSeenNotificationID));
-		setRequestBody(builder.build());
-	}
+class SaveMarkers(
+    lastSeenHomePostID: String?,
+    lastSeenNotificationID: String?
+) : MastodonAPIRequest<TimelineMarkers>(
+        method = HttpMethod.POST,
+        path = "/markers",
+        respClass = TimelineMarkers::class.java
+    ) {
+    init {
+        val builder = JsonObjectBuilder()
+
+        lastSeenHomePostID?.let {
+            builder.add(
+                key = "home",
+                el = JsonObjectBuilder().add("last_read_id", it)
+            )
+        }
+
+        lastSeenNotificationID?.let {
+            builder.add(
+                key = "notifications",
+                el = JsonObjectBuilder().add("last_read_id", it)
+            )
+        }
+
+        setRequestBody(builder.build())
+    }
 }
