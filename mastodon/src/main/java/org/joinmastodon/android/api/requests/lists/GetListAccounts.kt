bbdd476999b3b146f725018a28f2e0d7674bff06
FelipeIgansi <felipemigansi@gmail.com>
2025-07-19 18:02:34 -0300

    Bugfix: Changed GetListAccounts so that the maxID can be null by default

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/lists/GetListAccounts.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/lists/GetListAccounts.kt
@@ -6,13 +6,13 @@ import org.joinmastodon.android.api.requests.HeaderPaginationRequest
 import org.joinmastodon.android.model.Account
 import org.joinmastodon.android.model.HeaderPaginationList
 
-class GetListAccounts(listID: String?, maxID: String, limit: Int) :
+class GetListAccounts(listID: String?, maxID: String?, limit: Int) :
   HeaderPaginationRequest<Account?>(
     HttpMethod.GET,
     "/lists/$listID/accounts",
     object : TypeToken<HeaderPaginationList<Account?>?>() {}) {
   init {
-    if (!TextUtils.isEmpty(maxID)) addQueryParameter("max_id", maxID)
+    if (!TextUtils.isEmpty(maxID)) addQueryParameter("max_id", maxID.toString())
     addQueryParameter("limit", limit.toString())
   }
 }
