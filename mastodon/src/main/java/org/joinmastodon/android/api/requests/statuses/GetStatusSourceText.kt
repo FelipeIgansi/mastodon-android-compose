3ec275693cd35c4106af78b86bc38baadc5bfb47
FelipeIgansi <felipemigansi@gmail.com>
2025-07-20 20:40:50 -0300

    Migrate: GetStatusSourceText by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/GetStatusSourceText.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/GetStatusSourceText.kt
@@ -1,18 +1,23 @@
-package org.joinmastodon.android.api.requests.statuses;
+package org.joinmastodon.android.api.requests.statuses
 
-import org.joinmastodon.android.api.AllFieldsAreRequired;
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.BaseModel;
+import org.joinmastodon.android.api.AllFieldsAreRequired
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.BaseModel
 
-public class GetStatusSourceText extends MastodonAPIRequest<GetStatusSourceText.Response>{
-	public GetStatusSourceText(String id){
-		super(HttpMethod.GET, "/statuses/"+id+"/source", Response.class);
-	}
+class GetStatusSourceText(id: String) :
+  MastodonAPIRequest<GetStatusSourceText.Response>(
+    method = HttpMethod.GET,
+    path = "/statuses/$id/source",
+    respClass = Response::class.java
+  ) {
+  @AllFieldsAreRequired
+  class Response : BaseModel() {
+    var id: String? = null
 
-	@AllFieldsAreRequired
-	public static class Response extends BaseModel{
-		public String id;
-		public String text;
-		public String spoilerText;
-	}
+    @JvmField
+    var text: String? = null
+
+    @JvmField
+    var spoilerText: String? = null
+  }
 }
