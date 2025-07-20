7a3362871dfa80553b4f602a1bafbf767392757d
FelipeIgansi <felipemigansi@gmail.com>
2025-07-19 22:13:51 -0300

    Migrate: SetNotificationsPolicy by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/notifications/SetNotificationsPolicy.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/notifications/SetNotificationsPolicy.kt
@@ -1,11 +1,15 @@
-package org.joinmastodon.android.api.requests.notifications;
+package org.joinmastodon.android.api.requests.notifications
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.NotificationsPolicy;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.NotificationsPolicy
 
-public class SetNotificationsPolicy extends MastodonAPIRequest<NotificationsPolicy>{
-	public SetNotificationsPolicy(NotificationsPolicy policy){
-		super(HttpMethod.PUT, "/notifications/policy", NotificationsPolicy.class);
-		setRequestBody(policy);
-	}
+class SetNotificationsPolicy(policy: NotificationsPolicy) :
+    MastodonAPIRequest<NotificationsPolicy>(
+        method = HttpMethod.PUT,
+        path = "/notifications/policy",
+        respClass = NotificationsPolicy::class.java
+    ) {
+    init {
+        setRequestBody(policy)
+    }
 }
