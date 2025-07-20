7fe5beaf997b03bd7aa6869d55f09d0bc0ed8092
FelipeIgansi <felipemigansi@gmail.com>
2025-07-20 19:45:46 -0300

    Migrate: SetStatusFavorited by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/SetStatusFavorited.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/SetStatusFavorited.kt
@@ -1,11 +1,15 @@
-package org.joinmastodon.android.api.requests.statuses;
+package org.joinmastodon.android.api.requests.statuses
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Status;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Status
 
-public class SetStatusFavorited extends MastodonAPIRequest<Status>{
-	public SetStatusFavorited(String id, boolean favorited){
-		super(HttpMethod.POST, "/statuses/"+id+"/"+(favorited ? "favourite" : "unfavourite"), Status.class);
-		setRequestBody(new Object());
-	}
+class SetStatusFavorited(id: String, favorited: Boolean) :
+  MastodonAPIRequest<Status>(
+    method = HttpMethod.POST,
+    path = "/statuses/$id/${if (favorited) "favourite" else "unfavourite"}",
+    respClass = Status::class.java
+  ) {
+  init {
+    setRequestBody(Any())
+  }
 }
