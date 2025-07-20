aca4418bb579056f7fa773136613da8997b67540
FelipeIgansi <felipemigansi@gmail.com>
2025-07-20 19:56:47 -0300

    Migrate: SetStatusPinned by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/SetStatusPinned.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/SetStatusPinned.kt
@@ -1,11 +1,15 @@
-package org.joinmastodon.android.api.requests.statuses;
+package org.joinmastodon.android.api.requests.statuses
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Status;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Status
 
-public class SetStatusPinned extends MastodonAPIRequest<Status>{
-	public SetStatusPinned(String id, boolean pinned){
-		super(HttpMethod.POST, "/statuses/"+id+"/"+(pinned ? "pin" : "unpin"), Status.class);
-		setRequestBody(new Object());
-	}
+class SetStatusPinned(id: String, pinned: Boolean) :
+  MastodonAPIRequest<Status>(
+    method = HttpMethod.POST,
+    path = "/statuses/$id/${if (pinned) "pin" else "unpin"}",
+    respClass = Status::class.java
+  ) {
+  init {
+    setRequestBody(Any())
+  }
 }
