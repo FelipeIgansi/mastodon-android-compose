02e76ee6948e32679a0b69c30a6d51eabf821eab
FelipeIgansi <felipemigansi@gmail.com>
2025-07-20 20:00:57 -0300

    Migrate: SetStatusReblogged by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/SetStatusReblogged.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/SetStatusReblogged.kt
@@ -1,11 +1,15 @@
-package org.joinmastodon.android.api.requests.statuses;
+package org.joinmastodon.android.api.requests.statuses
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Status;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Status
 
-public class SetStatusReblogged extends MastodonAPIRequest<Status>{
-	public SetStatusReblogged(String id, boolean reblogged){
-		super(HttpMethod.POST, "/statuses/"+id+"/"+(reblogged ? "reblog" : "unreblog"), Status.class);
-		setRequestBody(new Object());
-	}
+class SetStatusReblogged(id: String, reblogged: Boolean) :
+  MastodonAPIRequest<Status>(
+    method = HttpMethod.POST,
+    path = "/statuses/$id/${if (reblogged) "reblog" else "unreblog"}",
+    respClass = Status::class.java
+  ) {
+  init {
+    setRequestBody(Any())
+  }
 }
