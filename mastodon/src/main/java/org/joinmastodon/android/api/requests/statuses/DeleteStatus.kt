e9cedea3cebec3099d9e1304984b7206b6b8533a
FelipeIgansi <felipemigansi@gmail.com>
2025-07-20 17:59:33 -0300

    Migrate: DeleteStatus by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/DeleteStatus.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/statuses/DeleteStatus.kt
@@ -1,10 +1,10 @@
-package org.joinmastodon.android.api.requests.statuses;
+package org.joinmastodon.android.api.requests.statuses
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Status;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Status
 
-public class DeleteStatus extends MastodonAPIRequest<Status>{
-	public DeleteStatus(String id){
-		super(HttpMethod.DELETE, "/statuses/"+id, Status.class);
-	}
-}
+class DeleteStatus(id: String) : MastodonAPIRequest<Status>(
+  method = HttpMethod.DELETE,
+  path = "/statuses/$id",
+  respClass = Status::class.java
+)
