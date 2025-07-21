b5820f4b2d32f023435664324362cb76d54cfede
FelipeIgansi <felipemigansi@gmail.com>
2025-07-20 23:07:58 -0300

    Migrate: SetTagFollowed by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/requests/tags/SetTagFollowed.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/requests/tags/SetTagFollowed.kt
@@ -1,11 +1,15 @@
-package org.joinmastodon.android.api.requests.tags;
+package org.joinmastodon.android.api.requests.tags
 
-import org.joinmastodon.android.api.MastodonAPIRequest;
-import org.joinmastodon.android.model.Hashtag;
+import org.joinmastodon.android.api.MastodonAPIRequest
+import org.joinmastodon.android.model.Hashtag
 
-public class SetTagFollowed extends MastodonAPIRequest<Hashtag>{
-	public SetTagFollowed(String tag, boolean followed){
-		super(HttpMethod.POST, "/tags/"+tag+(followed ? "/follow" : "/unfollow"), Hashtag.class);
-		setRequestBody(new Object());
-	}
+class SetTagFollowed(tag: String?, followed: Boolean) :
+  MastodonAPIRequest<Hashtag>(
+    method = HttpMethod.POST,
+    path = "/tags/$tag${if (followed) "/follow" else "/unfollow"}",
+    respClass = Hashtag::class.java
+  ) {
+  init {
+    setRequestBody(Any())
+  }
 }
