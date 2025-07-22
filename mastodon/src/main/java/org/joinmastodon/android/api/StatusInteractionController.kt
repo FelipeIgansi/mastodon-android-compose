8ffe95574c4ae7fe4297a86510b06a4b8794e758
FelipeIgansi <felipemigansi@gmail.com>
2025-07-22 14:09:35 -0300

    Migrate: StatusInteractionController by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/StatusInteractionController.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/StatusInteractionController.kt
@@ -1,133 +1,111 @@
-package org.joinmastodon.android.api;
+package org.joinmastodon.android.api
 
-import android.os.Looper;
+import android.os.Looper
+import me.grishka.appkit.api.Callback
+import me.grishka.appkit.api.ErrorResponse
+import org.joinmastodon.android.E.post
+import org.joinmastodon.android.MastodonApp
+import org.joinmastodon.android.api.requests.statuses.SetStatusBookmarked
+import org.joinmastodon.android.api.requests.statuses.SetStatusFavorited
+import org.joinmastodon.android.api.requests.statuses.SetStatusReblogged
+import org.joinmastodon.android.events.StatusCountersUpdatedEvent
+import org.joinmastodon.android.model.Status
 
-import org.joinmastodon.android.E;
-import org.joinmastodon.android.MastodonApp;
-import org.joinmastodon.android.api.requests.statuses.SetStatusBookmarked;
-import org.joinmastodon.android.api.requests.statuses.SetStatusFavorited;
-import org.joinmastodon.android.api.requests.statuses.SetStatusReblogged;
-import org.joinmastodon.android.events.StatusCountersUpdatedEvent;
-import org.joinmastodon.android.model.Status;
+class StatusInteractionController(private val accountID: String) {
 
-import java.util.HashMap;
+  private val runningFavoriteRequests = HashMap<String, SetStatusFavorited>()
+  private val runningReblogRequests = HashMap<String, SetStatusReblogged>()
+  private val runningBookmarkRequests = HashMap<String, SetStatusBookmarked>()
 
-import me.grishka.appkit.api.Callback;
-import me.grishka.appkit.api.ErrorResponse;
+  fun setFavorited(status: Status, favorited: Boolean) {
+    check(Looper.getMainLooper().isCurrentThread) { "Can only be called from main thread" }
 
-public class StatusInteractionController{
-	private final String accountID;
-	private final HashMap<String, SetStatusFavorited> runningFavoriteRequests=new HashMap<>();
-	private final HashMap<String, SetStatusReblogged> runningReblogRequests=new HashMap<>();
-	private final HashMap<String, SetStatusBookmarked> runningBookmarkRequests=new HashMap<>();
+    runningFavoriteRequests.remove(status.id)?.cancel()
 
-	public StatusInteractionController(String accountID){
-		this.accountID=accountID;
-	}
+    val request = SetStatusFavorited(status.id, favorited)
+      .setCallback(object : Callback<Status> {
 
-	public void setFavorited(Status status, boolean favorited){
-		if(!Looper.getMainLooper().isCurrentThread())
-			throw new IllegalStateException("Can only be called from main thread");
+        override fun onSuccess(result: Status) {
+          runningFavoriteRequests.remove(status.id)
+          post(StatusCountersUpdatedEvent(result))
+        }
 
-		SetStatusFavorited current=runningFavoriteRequests.remove(status.id);
-		if(current!=null){
-			current.cancel();
-		}
-		SetStatusFavorited req=(SetStatusFavorited) new SetStatusFavorited(status.id, favorited)
-				.setCallback(new Callback<>(){
-					@Override
-					public void onSuccess(Status result){
-						runningFavoriteRequests.remove(status.id);
-						E.post(new StatusCountersUpdatedEvent(result));
-					}
+        override fun onError(error: ErrorResponse) {
+          runningFavoriteRequests.remove(status.id)
+          error.showToast(MastodonApp.context)
+          status.favourited = !favorited
+          if (favorited) status.favouritesCount--
+          else status.favouritesCount++
+          post(StatusCountersUpdatedEvent(status))
+        }
+      })
+      .exec(accountID) as SetStatusFavorited
 
-					@Override
-					public void onError(ErrorResponse error){
-						runningFavoriteRequests.remove(status.id);
-						error.showToast(MastodonApp.context);
-						status.favourited=!favorited;
-						if(favorited)
-							status.favouritesCount--;
-						else
-							status.favouritesCount++;
-						E.post(new StatusCountersUpdatedEvent(status));
-					}
-				})
-				.exec(accountID);
-		runningFavoriteRequests.put(status.id, req);
-		status.favourited=favorited;
-		if(favorited)
-			status.favouritesCount++;
-		else
-			status.favouritesCount--;
-		E.post(new StatusCountersUpdatedEvent(status));
-	}
+    runningFavoriteRequests.put(status.id, request)
+    status.favourited = favorited
+    if (favorited) status.favouritesCount++
+    else status.favouritesCount--
 
-	public void setReblogged(Status status, boolean reblogged){
-		if(!Looper.getMainLooper().isCurrentThread())
-			throw new IllegalStateException("Can only be called from main thread");
+    post(StatusCountersUpdatedEvent(status))
+  }
 
-		SetStatusReblogged current=runningReblogRequests.remove(status.id);
-		if(current!=null){
-			current.cancel();
-		}
-		SetStatusReblogged req=(SetStatusReblogged) new SetStatusReblogged(status.id, reblogged)
-				.setCallback(new Callback<>(){
-					@Override
-					public void onSuccess(Status result){
-						runningReblogRequests.remove(status.id);
-						E.post(new StatusCountersUpdatedEvent(result));
-					}
+  fun setReblogged(status: Status, reblogged: Boolean) {
+    check(Looper.getMainLooper().isCurrentThread) { "Can only be called from main thread" }
 
-					@Override
-					public void onError(ErrorResponse error){
-						runningReblogRequests.remove(status.id);
-						error.showToast(MastodonApp.context);
-						status.reblogged=!reblogged;
-						if(reblogged)
-							status.reblogsCount--;
-						else
-							status.reblogsCount++;
-						E.post(new StatusCountersUpdatedEvent(status));
-					}
-				})
-				.exec(accountID);
-		runningReblogRequests.put(status.id, req);
-		status.reblogged=reblogged;
-		if(reblogged)
-			status.reblogsCount++;
-		else
-			status.reblogsCount--;
-		E.post(new StatusCountersUpdatedEvent(status));
-	}
+    runningReblogRequests.remove(status.id)?.cancel()
 
-	public void setBookmarked(Status status, boolean bookmarked){
-		if(!Looper.getMainLooper().isCurrentThread())
-			throw new IllegalStateException("Can only be called from main thread");
+    val request = SetStatusReblogged(status.id, reblogged)
+      .setCallback(object : Callback<Status> {
 
-		SetStatusBookmarked current=runningBookmarkRequests.remove(status.id);
-		if(current!=null){
-			current.cancel();
-		}
-		SetStatusBookmarked req=(SetStatusBookmarked) new SetStatusBookmarked(status.id, bookmarked)
-				.setCallback(new Callback<>(){
-					@Override
-					public void onSuccess(Status result){
-						runningBookmarkRequests.remove(status.id);
-						E.post(new StatusCountersUpdatedEvent(result));
-					}
+        override fun onSuccess(result: Status) {
+          runningReblogRequests.remove(status.id)
+          post(StatusCountersUpdatedEvent(result))
+        }
 
-					@Override
-					public void onError(ErrorResponse error){
-						runningBookmarkRequests.remove(status.id);
-						error.showToast(MastodonApp.context);
-						status.bookmarked=!bookmarked;
-						E.post(new StatusCountersUpdatedEvent(status));
-					}
-				})
-				.exec(accountID);
-		runningBookmarkRequests.put(status.id, req);
-		status.bookmarked=bookmarked;
-		E.post(new StatusCountersUpdatedEvent(status));
-	}
+        override fun onError(error: ErrorResponse) {
+          runningReblogRequests.remove(status.id)
+          error.showToast(MastodonApp.context)
+          status.reblogged = !reblogged
+          if (reblogged) status.reblogsCount--
+          else status.reblogsCount++
+          post(StatusCountersUpdatedEvent(status))
+        }
+      })
+      .exec(accountID) as SetStatusReblogged
+
+    runningReblogRequests.put(status.id, request)
+    status.reblogged = reblogged
+    if (reblogged) status.reblogsCount++
+    else status.reblogsCount--
+
+    post(StatusCountersUpdatedEvent(status))
+  }
+
+  fun setBookmarked(status: Status, bookmarked: Boolean) {
+    check(Looper.getMainLooper().isCurrentThread) { "Can only be called from main thread" }
+
+    runningBookmarkRequests.remove(status.id)?.cancel()
+
+    val request = SetStatusBookmarked(status.id, bookmarked)
+      .setCallback(object : Callback<Status> {
+
+        override fun onSuccess(result: Status) {
+          runningBookmarkRequests.remove(status.id)
+          post(StatusCountersUpdatedEvent(result))
+        }
+
+        override fun onError(error: ErrorResponse) {
+          runningBookmarkRequests.remove(status.id)
+          error.showToast(MastodonApp.context)
+          status.bookmarked = !bookmarked
+          post(StatusCountersUpdatedEvent(status))
+        }
+      })
+      .exec(accountID) as SetStatusBookmarked
+
+    runningBookmarkRequests.put(status.id, request)
+    status.bookmarked = bookmarked
+
+    post(StatusCountersUpdatedEvent(status))
+  }
 }
