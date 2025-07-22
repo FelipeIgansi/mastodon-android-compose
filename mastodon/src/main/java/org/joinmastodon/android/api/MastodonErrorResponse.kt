e87c1fd396c22a8ade13a11d350b57cba12bc186
FelipeIgansi <felipemigansi@gmail.com>
2025-07-22 11:49:32 -0300

    Migrate: MastodonErrorResponse by api path

--- mastodon/src/main/java/org/joinmastodon/android/api/MastodonErrorResponse.kt
+++ mastodon/src/main/java/org/joinmastodon/android/api/MastodonErrorResponse.kt
@@ -1,66 +1,52 @@
-package org.joinmastodon.android.api;
+package org.joinmastodon.android.api
 
-import android.content.Context;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.google.gson.JsonIOException
+import com.google.gson.JsonSyntaxException
+import me.grishka.appkit.api.ErrorResponse
+import org.joinmastodon.android.R
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;
+open class MastodonErrorResponse(
+  @JvmField val error: String?,
+  @JvmField val httpStatus: Int,
+  underlyingException: Throwable?
+) : ErrorResponse() {
 
-import org.joinmastodon.android.R;
+  val messageResource = when {
+    underlyingException is UnknownHostException -> R.string.could_not_reach_server
+    underlyingException is SocketTimeoutException -> R.string.connection_timed_out
 
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
+    underlyingException is JsonSyntaxException || underlyingException is JsonIOException ||
+        httpStatus >= 500 -> R.string.server_error
 
-import me.grishka.appkit.api.ErrorResponse;
+    httpStatus == 404 -> R.string.not_found
+    else -> 0
+  }
 
-public class MastodonErrorResponse extends ErrorResponse{
-	public final String error;
-	public final int httpStatus;
-	public final Throwable underlyingException;
-	public final int messageResource;
+  override fun bindErrorView(view: View) {
+    val text = view.findViewById<TextView>(R.id.error_text)
+    val message = getErrorMessage(view.context)
 
-	public MastodonErrorResponse(String error, int httpStatus, Throwable exception){
-		this.error=error;
-		this.httpStatus=httpStatus;
-		this.underlyingException=exception;
+    text.text = message
+  }
 
-		if(exception instanceof UnknownHostException){
-			this.messageResource=R.string.could_not_reach_server;
-		}else if(exception instanceof SocketTimeoutException){
-			this.messageResource=R.string.connection_timed_out;
-		}else if(exception instanceof JsonSyntaxException || exception instanceof JsonIOException || httpStatus>=500){
-			this.messageResource=R.string.server_error;
-		}else if(httpStatus==404){
-			this.messageResource=R.string.not_found;
-		}else{
-			this.messageResource=0;
-		}
-	}
+  override fun showToast(context: Context?) {
+    context?.let {
+      val message = getErrorMessage(it)
+      Toast.makeText(it, message, Toast.LENGTH_SHORT).show()
+    }
 
-	@Override
-	public void bindErrorView(View view){
-		TextView text=view.findViewById(R.id.error_text);
-		String message;
-		if(messageResource>0){
-			message=view.getContext().getString(messageResource, error);
-		}else{
-			message=error;
-		}
-		text.setText(message);
-	}
+  }
 
-	@Override
-	public void showToast(Context context){
-		if(context==null)
-			return;
-		String message;
-		if(messageResource>0){
-			message=context.getString(messageResource, error);
-		}else{
-			message=error;
-		}
-		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-	}
+  private fun getErrorMessage(context: Context): String {
+    return when {
+      messageResource > 0 -> context.getString(messageResource, error.orEmpty())
+      else -> error.orEmpty()
+    }
+  }
 }
